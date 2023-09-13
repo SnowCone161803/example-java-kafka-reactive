b5392c933658d927ecff6956ec7532a69d0f8ee3
Gordon Macrae <macraegordon@hotmail.com>
2023-09-13 19:00:18 +0100

    added event numbers to events as they are being handled

--- src/main/java/com/example/kafka/reactive/kafka/KafkaHandler.java
+++ src/main/java/com/example/kafka/reactive/kafka/KafkaHandler.java
@@ -1,5 +1,6 @@
 package com.example.kafka.reactive.kafka;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -8,27 +9,42 @@ import reactor.core.publisher.Sinks;
 
 import javax.annotation.PostConstruct;
 import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
 @Service
+@Slf4j
 public class KafkaHandler {
 
     // way too long
-    private static final Duration EVENT_TIMEOUIT = Duration.ofMinutes(5);
+    private static final Duration EVENT_TIMEOUT = Duration.ofMinutes(5);
 
     // replay everything everytime it has been subscribed to
     private final Sinks.Many<Function<Mono<KafkaEvent>, Mono<?>>> handlerSink = Sinks.many().replay().all();
 
+    private final AtomicInteger eventCount = new AtomicInteger(0);
+
     public void handle(KafkaEvent event) {
-        var calledHandlers = handlerSink
-            .asFlux()
-            .map((f) -> f.apply(Mono.just(event)));
-        Flux.merge(calledHandlers)
-            .blockLast(Duration.ofDays(1));
+        final int eventNumber = nextEventNumber();
+        log.info("event {} starting", eventNumber);
+        try {
+            var calledHandlers = handlerSink
+                .asFlux()
+                .map((f) -> f.apply(Mono.just(event)));
+            Flux.merge(calledHandlers)
+                .blockLast(EVENT_TIMEOUT);
+            log.info("event {} complete", eventNumber);
+        } catch (Exception ex) {
+            log.error("event {} failed", eventNumber);
+        }
     }
 
     public void addHandler(Function<Mono<KafkaEvent>, Mono<?>> handler) {
         handlerSink.tryEmitNext(handler);
+    }
+
+    private int nextEventNumber() {
+        return this.eventCount.getAndIncrement();
     }
 
     /**
