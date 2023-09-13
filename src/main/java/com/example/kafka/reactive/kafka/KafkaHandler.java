2d8184db14dde531aee2000a838efc3bdb3f800e
Gordon Macrae <macraegordon@hotmail.com>
2023-09-13 18:50:23 +0100

    first attempt at the handler

--- src/main/java/com/example/kafka/reactive/kafka/KafkaHandler.java
+++ src/main/java/com/example/kafka/reactive/kafka/KafkaHandler.java
@@ -2,23 +2,32 @@ package com.example.kafka.reactive.kafka;
 
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 
 import javax.annotation.PostConstruct;
+import java.time.Duration;
 import java.util.function.Function;
 
 @Service
 public class KafkaHandler {
 
-    private final Sinks.One<?> eventHandlerSink = Sinks.one();
+    // way too long
+    private static final Duration EVENT_TIMEOUIT = Duration.ofMinutes(5);
 
     // replay everything everytime it has been subscribed to
-    private final Sinks.Many<Function<KafkaEvent, ?>> handlerSink = Sinks.many().replay().all();
+    private final Sinks.Many<Function<Mono<KafkaEvent>, Mono<?>>> handlerSink = Sinks.many().replay().all();
 
     public void handle(KafkaEvent event) {
+        var calledHandlers = handlerSink
+            .asFlux()
+            .map((f) -> f.apply(Mono.just(event)));
+        Flux.merge(calledHandlers)
+            .blockLast(Duration.ofDays(1));
     }
 
-    public void addHandler(Function<KafkaEvent, ?> handler) {
+    public void addHandler(Function<Mono<KafkaEvent>, Mono<?>> handler) {
         handlerSink.tryEmitNext(handler);
     }
 
